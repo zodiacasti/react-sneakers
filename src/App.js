960dd620c63bdeccc8ecc506645aabf0311fe8c6
Karen Grigoryan <karen.grigoryan@emergn.com>
2021-10-01 00:02:14 +0300

    code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 function App() {
   return (
     <div className='wrapper clear'>
+      <div className='overlay'>
+        <div className='drawer'>
+          <h2>Cart</h2>
+        </div>
+      </div>
+
       <header className='d-flex justify-between align-center p-40'>
         <div className='d-flex align-center'>
           <img width={40} height={40} src='/img/logo.png' alt='logo' />
@@ -21,9 +27,21 @@ function App() {
       </header>
 
       <div className='content p-40'>
-        <h1 className='mb-40'>All sneakers</h1>
+        <div className='d-flex mb-40 align-center justify-between'>
+          <h1>All sneakers</h1>
+          <div>
+            <div className='search-block d-flex'>
+              <img src='/img/search.svg' alt='Search' />
+              <input placeholder='Search...' />
+            </div>
+          </div>
+        </div>
         <div className='d-flex'>
           <div className='card'>
+            <div className='favorite'>
+              <img src='img/heart-liked.svg' alt='liked' />
+            </div>
+
             <img width={133} height={112} src='/img/sneakers/1.jpg' alt='sneakers' />
             <h5>Men sneakers Nike Blazer Mid Suede</h5>
             <div className='d-flex justify-between align-center'>
@@ -31,9 +49,7 @@ function App() {
                 <span>Price:</span>
                 <b>150.00 EUR</b>
               </div>
-              <button className='button'>
-                <img width={11} height={11} src='/img/plus.svg' alt='plus' />
-              </button>
+              <img width={35} height={35} src='/img/plus.svg' alt='plus' />
             </div>
           </div>
           <div className='card'>
